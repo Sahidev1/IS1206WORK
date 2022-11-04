481263a2faa8dfefb134414c3423a5cc07c388eb
Sahidev1 <theoddrogue@gmail.com>
2022-11-04 20:44:49 +0100

    code cleanup

--- LAB1/part_1.c
+++ LAB1/part_1.c
@@ -4,57 +4,34 @@
 #include <string.h>
 
 int main (){
-    const int BUFFER_SIZE = 2048;
-    char write_buffer[BUFFER_SIZE];
-    char read_buffer[BUFFER_SIZE];
-    int status;
     int fd[2]; //File descriptors declaration
-
     pipe(fd); //Creating the pipe.
-    printf ("File descriptors: 0: %d, 1: %d \n", fd[0], fd[1]);
-
     pid_t pid = fork();
-
+    //printf ("File descriptors: 0: %d, 1: %d \n", fd[0], fd[1]);
     switch (pid)
     {
     case -1:
         printf ("Failed to to fork\n");
         break;
     case 0: //CHILD
-        close(fd[0]);
-        dup2(fd[1], 1); //piping stdout to fd[1]
-        close(fd[1]);
+        close(fd[0]); //closing read end.
+
+        //what would've gone to stdout goes to fd[1] instead, write end of the pipe.
+        dup2(fd[1], 1); 
+
         execlp ("ls", "ls", "/", NULL);
         break;
     default: //PARENT
-        close(fd[1]);
-
+        close(fd[1]); // closing write end.
         wait(NULL);
+
+        //what woud've been read from stdin is instead read from fd[0], read end of the pipe.
         dup2(fd[0],0);
+
         close(fd[0]);
-        //dprintf(fd[0], "hello");
-
         execlp("wc","wc","-l", NULL);
-        //dprintf(fd[0], "hello");
-        
-        //read(fd[0], read_buffer, BUFFER_SIZE);
-
-       // read(fd[0], read_buffer, BUFFER_SIZE);
-        //printf("%s", read_buffer);
-
         break;
     }
-
-   /* pid_t pid = fork();
-    if (pid == -1) printf ("Failed to create child process\n");
-    
-    if (pid == 0){
-        execlp("ls","ls", NULL);
-    }
-    else if (pid > 0){
-        wait(&status);
-    }
-    */
     return 0;
 }
 
