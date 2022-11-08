8ed4539668b5d8fb790205a596e5d1428a8608ae
Sahidev1 <theoddrogue@gmail.com>
2022-11-08 23:05:48 +0100

    minor

--- LAB1/build.sh
+++ LAB1/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e
 
 gcc part_1.c -o part_1.out
 gcc part_2_producer.c -o producer.out
 gcc part_2_consumer.c -o consumer.out
 
 echo "Build complete"
+
+echo $'Usage:\npart1: ./part_1.out\npart2: ./producer.out [FILEPATH]
+part2: ./consumer.out\n'
