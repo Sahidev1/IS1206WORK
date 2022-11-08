725dac87c6fbe30b7de07037f65d337a866ff551
Sahidev1 <theoddrogue@gmail.com>
2022-11-08 23:40:18 +0100

    part 2 producer error handling

--- LAB1/part_2_producer.c
+++ LAB1/part_2_producer.c
@@ -13,11 +13,12 @@
 #define BUFFER_SIZE 2048
 #define MAX_MSG_Q 10
 #define MQ_NAME "/derp"
+#define ERR_CODE 1
 
 int main (int argc, char *argv[]){
     if (argc != 2) {
         printf("Please enter 1 argument: the path to the text file!\n");
-        return -1;
+        return ERR_CODE;
     }
     char read_buffer[BUFFER_SIZE];
     ssize_t bytes_read;
@@ -29,22 +30,25 @@ int main (int argc, char *argv[]){
     fd = open(file_path, O_RDONLY);
     if (fd == -1) {
         perror("open() failed ");
-        return -1;
+        return ERR_CODE;
     }
 
     bytes_read = read(fd, read_buffer, BUFFER_SIZE);
     if (bytes_read == -1){
         perror("read() failed");
-        return -1;
+        return ERR_CODE;
     }
     close(fd);
 
     mqd = mq_open(MQ_NAME, O_CREAT | O_WRONLY, 0660, NULL);
     if (mqd == -1){
         perror("mq_open() failed");
-        return -1;
+        return ERR_CODE;
     }
-    mq_send(mqd, read_buffer, strlen(read_buffer) , 0);
+    if(mq_send(mqd, read_buffer, strlen(read_buffer) , 0) == -1){
+        perror("mq_send() failed");
+        return ERR_CODE;
+    }
     mq_close(mqd);
 
     return 0;
